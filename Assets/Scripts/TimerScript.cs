4da8b4f5ac7ea970a9fbea86f5389a6145fdab90
Th0masCat <catsaredogstoo@gmail.com>
2023-05-23 00:02:03 +0530

    finished clock and main scene

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class TimerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform hourHand;
+    public Transform minuteHand;
+    public Transform secondHand;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get the current system time
+        DateTime currentTime = DateTime.Now;
+
+        // Extract the hours, minutes, and seconds from the current time
+        int hours = currentTime.Hour;
+        int minutes = currentTime.Minute;
+        int seconds = currentTime.Second;
+
+        // Calculate the rotation angles for the clock hands
+        float hourAngle = 360f * ((hours % 12) + minutes / 60f) / 12f;
+        float minuteAngle = 360f * ((minutes % 60) + seconds / 60f) / 60f;
+        float secondAngle = 360f * (seconds % 60) / 60f;
+
+        // Apply the rotations to the clock hands
+        hourHand.localRotation = Quaternion.Euler(0f, hourAngle, 0f);
+        minuteHand.localRotation = Quaternion.Euler(0f, minuteAngle, 0f);
+        secondHand.localRotation = Quaternion.Euler(0f, secondAngle, 0f);
     }
 }
